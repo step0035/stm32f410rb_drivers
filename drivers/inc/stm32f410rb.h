2443ebabf990a5b51de6db227f1bca84950925f9
step0035 <step0035@e.ntu.edu.sg>
2021-06-27 01:21:46 +0800

    added GPIO macros to stm32f410rb.h

--- drivers/inc/stm32f410rb.h
+++ drivers/inc/stm32f410rb.h
@@ -39,4 +39,80 @@
  * peripheral data structures
  */
 
+/*
+ * GPIO
+ */
 
+typedef struct {
+    volatile uint32_t MODER;
+    volatile uint32_t OTYPER;
+    volatile uint32_t OSPEEDER;
+    volatile uint32_t PUPDR;
+    volatile uint32_t IDR;
+    volatile uint32_t ODR;
+    volatile uint32_t BSRR;
+    volatile uint32_t LCKR;
+    volatile uint32_t AFR[2];
+} GPIO_RegDef_t;
+
+/*
+ * RCC
+ */
+
+typedef struct {
+    volatile uint32_t CR;
+    volatile uint32_t PLLCFGR;
+    volatile uint32_t CFGR;
+    volatile uint32_t CIR;
+    volatile uint32_t AHB1RSTR;
+    uint32_t RSVD0[3];
+    volatile uint32_t APB1STR;
+    volatile uint32_t APB2STR;
+    uint32_t RSVD1[2];
+    volatile uint32_t AHB1ENR;
+    uint32_t RSVD2[3];
+    volatile uint32_t APB1ENR;
+    volatile uint32_t APB2ENR;
+    uint32_t RSVD3[2];
+    volatile uint32_t AHB1LPENR;
+    uint32_t RSVD4[3];
+    volatile uint32_t APB1LPENR;
+    volatile uint32_t APB2LPENR;
+    uint32_t RSVD5[2];
+    volatile uint32_t BDCR;
+    volatile uint32_t CSR;
+    uint32_t RSVD6[2];
+    volatile uint32_t SSCGR;
+    uint32_t RSVD7[2];
+    volatile uint32_t DCKCFGR;
+    uint32_t RSVD8;
+    volatile uint32_t DCKCFGR2;
+} RCC_RegDef_t;
+
+/**************************************************
+ * Definitions
+ */
+
+/*
+ * GPIO
+ */
+
+#define GPIOA  				((GPIO_RegDef_t*) GPIOA_BASEADDR)
+#define GPIOB  				((GPIO_RegDef_t*) GPIOB_BASEADDR)
+#define GPIOC  				((GPIO_RegDef_t*) GPIOC_BASEADDR)
+#define GPIOH  				((GPIO_RegDef_t*) GPIOH_BASEADDR)
+
+#define GPIOA_PCLK_EN()      (RCC -> AHB1ENR |= (1 << 0))
+#define GPIOB_PCLK_EN()      (RCC -> AHB1ENR |= (1 << 1))
+#define GPIOC_PCLK_EN()      (RCC -> AHB1ENR |= (1 << 2))
+#define GPIOH_PCLK_EN()      (RCC -> AHB1ENR |= (1 << 7))
+
+#define GPIOA_PCLK_DI()      (RCC -> AHB1ENR &= (~(1 << 0)))
+#define GPIOB_PCLK_DI()      (RCC -> AHB1ENR &= (~(1 << 1)))
+#define GPIOC_PCLK_DI()      (RCC -> AHB1ENR &= (~(1 << 2)))
+#define GPIOH_PCLK_DI()      (RCC -> AHB1ENR &= (~(1 << 7)))
+
+#define GPIOA_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 0)); (RCC->AHB1RSTR &= ~(1 << 0)); }while(0)
+#define GPIOB_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 1)); (RCC->AHB1RSTR &= ~(1 << 1)); }while(0)
+#define GPIOC_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 2)); (RCC->AHB1RSTR &= ~(1 << 2)); }while(0)
+#define GPIOH_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 7)); (RCC->AHB1RSTR &= ~(1 << 7)); }while(0)
