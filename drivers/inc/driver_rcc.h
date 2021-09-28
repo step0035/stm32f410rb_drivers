d19f179ece211baa2363e1ebfab1a773186d64ad
step0035 <step0035@e.ntu.edu.sg>
2021-09-28 23:33:12 +0800

    Add RCC drivers

---
+++ drivers/inc/driver_rcc.h
@@ -0,0 +1,14 @@
+#ifdef DRIVER_RCC_H_
+#define DRIVER_RCC_H_
+
+#include "stm32f410rb.h"
+
+//This returns the APB1 clock value
+uint32_t RCC_GetPCLK1Value(void);
+
+//This returns the APB2 clock value
+uint32_t RCC_GetPCLK2Value(void);
+
+uint32_t  RCC_GetPLLOutputClock(void);
+
+#endif
